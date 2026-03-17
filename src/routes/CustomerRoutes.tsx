e15f3dc9f693480a6476021b9aafb7dce5ec09a7
Kartik Bhadouriya <ks8783375@gmail.com>
2026-03-17 23:02:52 +0530

    Update CustomerRoutes.tsx

--- src/routes/CustomerRoutes.tsx
+++ src/routes/CustomerRoutes.tsx
@@ -1,3 +1,79 @@
+// import { Route, Routes } from 'react-router-dom'
+// import Home from '../customer/pages/Home/Home'
+// import Products from '../customer/pages/Products/Products'
+// import ProductDetails from '../customer/pages/Products/ProductDetails/ProductDetails'
+// import Cart from '../customer/pages/Cart/Cart'
+// import Address from '../customer/pages/Checkout/AddressPage'
+// import Profile from '../customer/pages/Account/Profile'
+// import Footer from '../customer/components/Footer/Footer'
+// import Navbar from '../customer/components/Navbar/Navbar'
+// import NotFound from '../customer/pages/NotFound/NotFound'
+// import Auth from '../customer/pages/Auth/Auth'
+// import { useAppDispatch, useAppSelector } from '../Redux Toolkit/Store'
+// import { fetchUserCart } from '../Redux Toolkit/Customer/CartSlice'
+// import PaymentSuccessHandler from '../customer/pages/Pyement/PaymentSuccessHandler'
+// import Reviews from '../customer/pages/Review/Reviews'
+// import WriteReviews from '../customer/pages/Review/WriteReview'
+// import Wishlist from '../customer/pages/Wishlist/Wishlist'
+// import BuyNowCheckoutPage from '../customer/pages/Checkout/BuyNowCheckoutPage'
+// import { getWishlistByUserId } from '../Redux Toolkit/Customer/WishlistSlice'
+// import SearchProducts from '../customer/pages/Search/SearchProducts'
+// import { useEffect } from 'react'
+// import FooterContent from '../customer/components/Footer/FooterContent'
+// import Contact from '../customer/components/Footer/Contact'
+// import DesktopPopup from '../customer/popup/DesktopPopup'
+
+
+
+// const CustomerRoutes = () => {
+//   const dispatch = useAppDispatch()
+//   const { auth } = useAppSelector(store => store);
+
+//   useEffect(() => {
+//     dispatch(fetchUserCart(localStorage.getItem("jwt") || ""))
+//     dispatch(getWishlistByUserId())
+//   }, [auth.jwt])
+//   return (
+//     <>
+//       <Navbar />
+//        <DesktopPopup
+//   initialImageUrl="https://cdn.vectorstock.com/i/1000v/19/10/cosmetics-beauty-products-for-make-up-sale-banner-vector-38391910.jpg"
+//   allowedMobile="9992088843" // sirf ye number se upload ho
+//   delay={10000} // 10 sec
+
+// />
+//       <Routes>
+//         <Route path='/' element={<Home />} />
+//         {/* <Route path='/chat-bot' element={<ChatBot />} /> */}
+//         <Route path='/products/:categoryId' element={<Products />} />
+//         <Route path='/search-products' element={<SearchProducts />} />
+//         <Route path='/reviews/:productId' element={<Reviews />} />
+//         <Route path='/reviews/:productId/create' element={<WriteReviews />} />
+//         <Route path='/product-details/:categoryId/:name/:productId' element={<ProductDetails />} />
+//         <Route path='/cart' element={<Cart />} />
+//         <Route path='/wishlist' element={<Wishlist />} />
+//         <Route path='/checkout/address' element={<Address />} />
+//         <Route path='/checkout/buy-now' element={<BuyNowCheckoutPage />} />
+//         <Route path='/account/*' element={<Profile />} />
+//         <Route path='/login' element={<Auth />} />
+//         <Route path='/payment-success/:orderId' element={<PaymentSuccessHandler />} />
+//         <Route path="/:slug" element={<FooterContent />} />
+//         <Route path='*' element={<NotFound />} />
+//         <Route path='/contact' element={<Contact/>}/>
+        
+//       </Routes>
+//       <Footer />
+//     </>
+
+
+
+
+
+
+
+
+
+
 import { Route, Routes } from 'react-router-dom'
 import Home from '../customer/pages/Home/Home'
 import Products from '../customer/pages/Products/Products'
@@ -23,28 +99,31 @@ import FooterContent from '../customer/components/Footer/FooterContent'
 import Contact from '../customer/components/Footer/Contact'
 import DesktopPopup from '../customer/popup/DesktopPopup'
 
-
-
 const CustomerRoutes = () => {
   const dispatch = useAppDispatch()
-  const { auth } = useAppSelector(store => store);
+  const { auth } = useAppSelector(store => store)
 
+  // ✅ FIXED useEffect
   useEffect(() => {
-    dispatch(fetchUserCart(localStorage.getItem("jwt") || ""))
-    dispatch(getWishlistByUserId())
-  }, [auth.jwt])
+    if (auth.jwt) {
+      dispatch(fetchUserCart(auth.jwt))
+      dispatch(getWishlistByUserId())
+    }
+  }, [auth.jwt, dispatch])
+
   return (
     <>
       <Navbar />
-       <DesktopPopup
-  initialImageUrl="https://cdn.vectorstock.com/i/1000v/19/10/cosmetics-beauty-products-for-make-up-sale-banner-vector-38391910.jpg"
-  allowedMobile="9992088843" // sirf ye number se upload ho
-  delay={10000} // 10 sec
 
-/>
+      {/* ✅ Popup only once logic handled in component */}
+      <DesktopPopup
+        initialImageUrl="https://cdn.vectorstock.com/i/1000v/19/10/cosmetics-beauty-products-for-make-up-sale-banner-vector-38391910.jpg"
+        allowedMobile="9992088843"
+        delay={10000}
+      />
+
       <Routes>
         <Route path='/' element={<Home />} />
-        {/* <Route path='/chat-bot' element={<ChatBot />} /> */}
         <Route path='/products/:categoryId' element={<Products />} />
         <Route path='/search-products' element={<SearchProducts />} />
         <Route path='/reviews/:productId' element={<Reviews />} />
@@ -57,15 +136,38 @@ const CustomerRoutes = () => {
         <Route path='/account/*' element={<Profile />} />
         <Route path='/login' element={<Auth />} />
         <Route path='/payment-success/:orderId' element={<PaymentSuccessHandler />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/:slug" element={<FooterContent />} />
         <Route path='*' element={<NotFound />} />
-        <Route path='/contact' element={<Contact/>}/>
-        
       </Routes>
+
       <Footer />
     </>
-
   )
 }
 
 export default CustomerRoutes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   )
+// }
+
+// export default CustomerRoutes
